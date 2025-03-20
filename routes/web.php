ae39e0afda22875d63a3a587240eb5d72d00aae0
Firli_Hanifurahman <firlihanifurahman753@gmail.com>
2025-03-20 10:26:24 +0700

    aku donlot breeze untuk keperluan login

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ListBarangController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/user/{id}', function ($id) {
-    return 'User dengan ID ' . $id ;
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
 });
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', function () {
-        return 'Selamat Datang Sosok Admin Dashboard';
-    });
 
-    Route::get('/users', function() {
-        return 'Selamat Datang Sosok Admin Users';
-    });
-
-});
-//Route::get('/listbarang/{id}/{nama}', function($id, $nama){
-//    return view('list_barang', compact('id', 'nama'));
-//});
-
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/contact', [HomeController::class, 'contact']);
-Route::get('/listbarang', [ListBarangController::class, 'tampilkan']);
+require __DIR__.'/auth.php';
